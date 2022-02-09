0788aa41e8633c317bc4941d8b491d441abbcd90
Dmitry <filyuta.dima@yandex.ru>
2022-02-09 23:09:51 +0300

    Project created

---
+++ UITableView/UITableView/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Dmitry on 9.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
